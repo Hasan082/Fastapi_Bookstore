cc8e16da588d9b69c2fb5230f21a4b42ddabdb28
Md Hasanuzzaman <dr.has82@gmail.com>
2024-06-17 23:54:29 +0600

    [UPDATE] Update full Project

---
+++ books.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request, Response
+
+app = FastAPI()
+
+BOOKS = [
+    {'title': 'title 1', 'author': 'Author 1', 'category': 'Math'},
+    {'title': 'title 2', 'author': 'Author 2', 'category': 'Science'},
+    {'title': 'title 3', 'author': 'Author 3', 'category': 'Science'},
+    {'title': 'title 4', 'author': 'Author 4', 'category': 'History'},
+    {'title': 'title 5', 'author': 'Author 5', 'category': 'Science'},
+    {'title': 'title 6', 'author': 'Author 6', 'category': 'History'},
+]
+
+
+@app.get("/")
+def books():
+    return BOOKS
